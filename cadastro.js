27a416fe873e50c41f29224e2e33c169f56f099f
Gabriela Cantarini <89015121+gabicantarini@users.noreply.github.com>
2021-12-01 10:43:20 +0000

    first commit interface controll

---
+++ cadastro.js
@@ -0,0 +1,14 @@
+
+const formulario = document.getElementById("formulario")
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const cadastroCliente = {
+        model: document.getElementById("name")
+        label: 
+        type: 
+        owner: 
+        description: 
+    }
+    console.log(event)
+})
