fce53914dc8d5097bd12eef5b5c5a26978ae6e2c
Gabriela Cantarini <89015121+gabicantarini@users.noreply.github.com>
2021-12-01 10:44:37 +0000

    finished inputing activities

--- activitiesController.js
+++ activitiesController.js
@@ -36,47 +36,51 @@ export const activityCheckin = async (request, response) => {
 
 export const activityCheckout = async (request, response) => {
     const { label, price } = request.body; /*manda a placa do carro no checkin*/
-
     const db = await openDatabase();
     /*verifica se a placa existe*/
+
     const vehicle = await db.get(`
-        SELECT * FROM vehicles WHERE label = ?
+        SELECT * 
+            FROM vehicles 
+           WHERE label = ?
         `, [label]);
 
-        if (vehicle) ({ /*se existir o vehicle cadastrado*/
-            /*variavel*//*pega a data atual e executa*/
-            const activityOpen = await db.get(`
-                SELECT * 
-                 FROM activities 
-                WHERE vehicle_id = ?
+    if (vehicle) { /*se existir o vehicle cadastrado*/
+        /*variavel*//*pega a data atual e executa*/
+        const activityOpen = await db.get(`
+            SELECT * 
+                FROM activities 
+               WHERE vehicle_id = ?
                  AND checkout_at IS NULL
         `, [vehicle.id]);
         
-            if (activityOpen) { /*se tiver atividade*/
-                const checkoutAt = (new Date()).getTime(); 
-                /*executa a atividade*/
-                const data = await db.run(` 
-                    UPDATE activities 
-                        SET checkout_at = ?,
-                            price = ?
-                    WHERE id = ?
-                `, [checkoutAt, price, activityOpen.id]);
-                /*fecha atividade*/
-                db.close();  
-                /*responde pra api*/
-                response.send({
-                    vehicle_id: vehicle.id,
-                    checkin_at: checkoutAt,
-                    price: price,
-                    message: 'Veículo [§{vehicle.label}] saiu do estacionamento',
-            
-                });
-                return;
-            db.close(); /* se não tiver o veículo, ele vai fechar e dizer que não foi cadastrado*/
+        if (activityOpen) { /*se tiver atividade*/
+            const checkoutAt = (new Date()).getTime(); 
+            /*executa a atividade*/
+            const data = await db.run(` 
+                UPDATE activities 
+                    SET checkout_at = ?,
+                        price = ?
+                 WHERE id = ?
+            `, [checkoutAt, price, activityOpen.id]);
+            /*fecha atividade*/
+            db.close();  
+            /*responde pra api*/
             response.send({
-                message: 'Veículo [§{label}] não realizou nenhum check-in' 
-            }); 
+                vehicle_id: vehicle.id,
+                checkin_at: checkoutAt,
+                price: price,
+                message: 'Veículo [§{vehicle.label}] saiu do estacionamento',
+            
+            });
             return;
+        }
+        /* se não tiver o veículo, ele vai fechar e dizer que não foi cadastrado*/
+        db.close();
+        response.status(400).send({
+            message: `Veículo [${label}] não realizou nenhum check-in`
+        });
+        return;
     }
 
     db.close(); /* se não tiver o veículo, ele vai fechar e dizer que não foi cadastrado*/
@@ -84,7 +88,7 @@ export const activityCheckout = async (request, response) => {
         message: 'Veículo [§{label}] não cadastrado' 
         
     });
-};
+}
 
 export const removeActivity = async (request, response) => {
     const { id } = request.params;
