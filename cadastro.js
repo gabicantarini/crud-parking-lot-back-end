68c200f1b924e93f56f75411e48eb00c4f8ef9bf
Gabriela Cantarini <89015121+gabicantarini@users.noreply.github.com>
2021-12-03 10:55:27 +0000

    form api validation

--- cadastro.js
+++ cadastro.js
@@ -1,14 +1,16 @@
+import { postVeiculo } from "../service/index.js";
 
 const formulario = document.getElementById("formulario")
 formulario.addEventListener('submit', function(event) {
     event.preventDefault()
 
     const cadastroCliente = {
-        model: document.getElementById("name")
-        label: 
-        type: 
-        owner: 
-        description: 
+        owner: document.getElementById("nome").value,
+        model: document.getElementById("modelo").value,
+        label: document.getElementById("placa").value,
+        type: document.getElementById("tipo").value,
+        description: document.getElementById("observacoes").value,
     }
-    console.log(event)
-})
+    postVeiculo(cadastroCliente)
+    //console.log(cadastroCliente)
+});
