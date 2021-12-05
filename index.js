dbe4065334f32344d2cbd372bf07c040a2122761
Gabriela Cantarini <89015121+gabicantarini@users.noreply.github.com>
2021-12-05 17:01:05 +0000

    finished

--- index.js
+++ index.js
@@ -1,119 +1,31 @@
+import { CadastroComponent } from "../controller/cadastro.js";
+import { FaturamentoComponent } from "../controller/faturamento.js";
+import { ListaClienteComponent } from "../controller/lista-Clientes.js";
+import { view } from "../view/index.js";
 
-const url = "https://traco-3.herokuapp.com/api"
-// ------------- POST ----------------------
-const postVeiculo = (objetoCliente) => {
-    console.log("objetoCliente: ", JSON.stringify(objetoCliente))
-    return fetch(url + "/vehicles", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(objetoCliente) 
-    }).then((response) => {
-       if(response.status != 200){
-           console.log(`Erro no servidor: ${response.status}`)
-       }else {
-           alert(`Sucesso ao salvar: ${response.status}`)
-       }
-    })
-}
+view.getSpinner()
+    setTimeout(() => {
+        view.getIndex();
+    }, 600)
 
-const postCheckin = (label) => {
-    return fetch(url + "/activities/checkin", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({label}) 
-    }).then((response) => {
-       if(response.status != 200){
-           console.log(`Erro no servidor: ${response.status}`)
-       }else {
-           return response.json()
-       }
-    })
-}
+const link = document.getElementById('link')
 
+link.addEventListener('click', (event) => {
+    const option = event.path[0].innerText
 
-// ------------ GET ---------------------------
-const getVeiculo = () => {
-    return fetch(url + "/vehicles")
-    .then((response) => {
-        if(response.status != 200){
-            console.log(`Erro no servidor: ${response.status}`)
-        }else {
-            return response.json()
-        }
-     })
-}
-
-const getActivities = () => {
-    return fetch(url + "/activities")
-    .then((response) => {
-        if(response.status != 200){
-            console.log(`Erro no servidor: ${response.status}`)
-        }else {
-            return response.json()
-        }
-     })
-}
-//------------ PUT -------------------------------
-
-const putVeiculo = (objetoCliente, id) => {
-    return fetch(`${url}/vehicles/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(objetoCliente) 
-    }).then((response) => {
-       if(response.status != 200){
-           console.log(`Erro no servidor: ${response.status}`)
-       }else {
-          return response.json()
-       }
-    })
-}
-
-const putCheckout = (objeto) => {
-    return fetch(url + "/activities/checkout", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(objeto) 
-    }).then((response) => {
-       if(response.status != 200){
-           console.log(`Erro no servidor: ${response.status}`)
-       }else {
-           return response.json()
-       }
-    })
-}
-
-// -------------- DELETE -------------------
-const deletaVeiculo = (id) => {
-    return fetch(`${url}/vehicles/${id}`, {
-        method: "DELETE",
-    }).then((response) => {
-       if(response.status != 200){
-           console.log(`Erro no servidor: ${response.status}`)
-       }else {
-          return response.json()
-       }
-    })
-}
-
-//organiação da exportação
-// -------------- EXPORT --------------------
-export const service = {
-    postVeiculo,
-    getVeiculo,
-    putVeiculo,
-    deletaVeiculo,
-    getActivities,
-    postCheckin,
-    putCheckout
-}
-
-
+    switch(option){
+        case "Cadastro":
+            CadastroComponent();
+            break
+        case "Clientes":
+            ListaClienteComponent();
+            break
+        case "Checkin": 
+            window.location.href = "./checkin.html"
+            break
+        case "Faturamento":
+            FaturamentoComponent();
+            break;
+        default : view.getIndex()
+    }
+})
