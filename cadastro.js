1d36023b75294b9b2e97c3bc2321c4caad684eed
Gabriela Cantarini <89015121+gabicantarini@users.noreply.github.com>
2021-12-05 17:05:49 +0000

    finished

--- cadastro.js
+++ cadastro.js
@@ -1,16 +1,47 @@
-import { postVeiculo } from "../service/index.js";
+import { service } from "../service/index.js"
+import { view } from "../view/index.js"
 
-const formulario = document.getElementById("formulario")
-formulario.addEventListener('submit', function(event) {
-    event.preventDefault()
+export const CadastroComponent = () => {
+    view.getSpinner(); //configuração dos milissegundos do spinner
 
-    const cadastroCliente = {
-        owner: document.getElementById("nome").value,
-        model: document.getElementById("modelo").value,
-        label: document.getElementById("placa").value,
-        type: document.getElementById("tipo").value,
-        description: document.getElementById("observacoes").value,
-    }
-    postVeiculo(cadastroCliente)
-    //console.log(cadastroCliente)
-});
+    setTimeout(() => {
+        const label = []
+        service.getVeiculo().then((dados) => {
+            dados.forEach(element => {
+                if (element.label != null) {
+                    label.push(element.label)
+                }
+            });
+        })
+
+        view.getCadastroHtml();
+
+        const formulario = document.getElementById("formulario")
+        formulario.addEventListener('submit', function (event) {
+            event.preventDefault()
+
+            const cadastroCliente = {
+                owner: document.getElementById('name').value,
+                model: document.getElementById('modelo').value,
+                type: document.getElementById('tipo').value,
+                label: document.getElementById('placa').value,
+                observation: document.getElementById('observacoes').value
+            }
+
+            if (label.includes(cadastroCliente.label)) {
+                return alert(`Essa placa: ${cadastroCliente.label} já existe no banco.`)
+            } else {
+                formulario.reset()
+                service.postVeiculo(cadastroCliente)
+            }
+        })
+
+        const cancelar = document.getElementById("cancelar");
+        cancelar.addEventListener('click', (event) => {
+            event.preventDefault();
+            formulario.reset();
+        })
+    }, 600)
+
+
+}
